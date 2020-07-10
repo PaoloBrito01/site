afb2295f5b9c795e63971020253c4b782d339a60
Paolo Brito <fuviolinux@gmail.com>
2020-07-10 15:33:17 -0400

    colocando sistema de login

---
+++ cadastro.php
@@ -0,0 +1,51 @@
+<?php 
+ 
+$login = $_POST['login'];
+$senha = MD5($_POST['senha']);
+$connect = mysql_connect('nome_do_servidor','nome_de_usuario','senha');
+$db = mysql_select_db('nome_do_banco_de_dados');
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = mysql_query($query_select,$connect);
+$array = mysql_fetch_array($select);
+$logarray = $array['login'];
+ 
+  if($login == "" || $login == null){
+    echo"<script language='javascript' type='text/javascript'>
+    alert('O campo login deve ser preenchido');window.location.href='
+    cadastro.html';</script>";
+ 
+    }else{
+      if($logarray == $login){
+ 
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Esse login já existe');window.location.href='
+        cadastro.html';</script>";
+        die();
+ 
+      }else{
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
+        $insert = mysql_query($query,$connect);
+         
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Usuário cadastrado com sucesso!');window.location.
+          href='login.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Não foi possível cadastrar esse usuário');window.location
+          .href='cadastro.html'</script>";
+        }
+      }
+    }
+?>
+<?php
+  $login_cookie = $_COOKIE['login'];
+    if(isset($login_cookie)){
+      echo"Bem-Vindo, $login_cookie <br>";
+      echo"Essas informações <font color='red'>PODEM</font> ser acessadas por você";
+    }else{
+      echo"Bem-Vindo, convidado <br>";
+      echo"Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você";
+      echo"<br><a href='login.html'>Faça Login</a> Para ler o conteúdo";
+    }
+?>
